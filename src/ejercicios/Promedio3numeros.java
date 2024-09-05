c7f73d1074c861915e9e2a6482ec154ab0c0e740
Juan Gutierrez <juan.gutierrez@tsoftglobal.com>
2024-09-05 17:07:20 -0400

    actualización 4

---
+++ src/ejercicios/Promedio3numeros.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class Promedio3numeros {
+    // Método principal
+    public static void main(String[] args) {
+        // Definir los números
+        int a = 4;
+        int b = 9;
+        int c = 13;
+
+        // Calcular y mostrar el promedio
+        System.out.println("El promedio es " + ((a + b + c) / 3));
+    }
+}
