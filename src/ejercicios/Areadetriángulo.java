a0a2f01532460c241f5bb4185e7e48034721f941
Juan Gutierrez <juan.gutierrez@tsoftglobal.com>
2024-09-05 17:21:02 -0400

    actualización 4

---
+++ src/ejercicios/Areadetriángulo.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Areadetriángulo {
+    // Método principal
+    public static void main(String[] args) {
+        // Definir la base y la altura
+        int b = 5;
+        int h = 8;
+
+        // Calcular y mostrar el área del triángulo
+        System.out.println("El área del triángulo es " + (b * h / 2.0));
+    }
+}
