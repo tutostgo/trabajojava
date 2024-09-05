b3eeb108ea8d271768fc69d13bcb5deeefa5dbe4
Juan Gutierrez <juan.gutierrez@tsoftglobal.com>
2024-09-05 16:25:47 -0400

    actualización 1

---
+++ src/ejercicios/Númeroparimpar.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+public class Númeroparimpar {
+    // Método principal
+    public static void main(String[] args) {
+        // Definir el número
+        int a = 15;
+
+        // Determina si el número es par o impar
+        if (a % 2 == 0) {
+        // muestra el resultado
+            System.out.println(a + " es un número par.");
+        } else {
+        // muestra el resultado
+            System.out.println(a + " es un número impar.");
+        }
+    }
+}
--- src/ejercicios/Productodosnúmeros.java
+++ src/ejercicios/Productodosnúmeros.java
@@ -1,5 +1,21 @@
 package ejercicios;
 
 public class Productodosnúmeros {
+// Método principal
+    public static void main(String[] args) {
+        // Definir dos números
+        int a = 7;
+        int b = 8;
 
+        // Llamar al método que saca producto
+        int resultado = multiplicar(a, b);
+
+        // Imprimir
+        System.out.println("Producto es " + a + " y " + b + " es " + resultado);
+    }
+
+    // Método que multiplica el producto
+    public static int multiplicar(int c, int d) {
+        return c * d;
+    }
 }
