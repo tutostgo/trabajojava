f2cd4702901adce0c374b77e31e8543ac57af25c
Juan Gutierrez <juan.gutierrez@tsoftglobal.com>
2024-09-06 12:47:55 -0400

    actualización 4

---
+++ src/ejercicios/Conversióntemperatura.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Conversióntemperatura {
+    // Método principal
+    public static void main(String[] args) {
+        // Definir la temperatura en Celsius
+        int C = 25;
+
+        // Convertir y mostrar la temperatura en Fahrenheit
+        System.out.println(C + " °C es igual a " + ((C * 9.0 / 5) + 32) + " °F");
+    }
+}
