db81da5c096e2cd178bb7625f207f0b8d12b53f0
Juan Gutierrez <juan.gutierrez@tsoftglobal.com>
2024-09-06 12:59:30 -0400

    actualización 4

---
+++ src/ejercicios/SumaprimerosNnumnaturales.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class SumaprimerosNnumnaturales {
+    // Método principal
+    public static void main(String[] args) {
+        // Definir el valor de N
+        int N = 50;
+
+        // Calcula y muestra la suma de los primeros N números naturales
+        System.out.println("Aqui va la suma con los primeros " + N + " números naturales es " + (N * (N + 1) / 2));
+    }
+}
