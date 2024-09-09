218b1fa776a631823409258a0a26ba00370327e5
Juan Gutierrez <juan.gutierrez@tsoftglobal.com>
2024-09-09 10:14:47 -0300

    Clase para imprimir valor absoluto

---
+++ src/ejercicios/Valorabsoluto.java
@@ -0,0 +1,8 @@
+package ejercicios;
+
+public class Valorabsoluto {
+    public static void main(String[] args) {
+        // Se imprime directamente el valor absoluto del número -15, para simpliifcar la solución
+        System.out.println("El valor absoluto de -15 es " + Math.abs(-15));
+    }
+}
