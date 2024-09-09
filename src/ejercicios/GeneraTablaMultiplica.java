2e4e81d6be922e09e0efaf2acec046db17b985a1
Juan Gutierrez <juan.gutierrez@tsoftglobal.com>
2024-09-09 10:08:31 -0300

    Clase para Generar Tabla Multiplicación del 7

---
+++ src/ejercicios/GeneraTablaMultiplica.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class GeneraTablaMultiplica {
+    public static void main(String[] args) {
+        int numero = 7;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
